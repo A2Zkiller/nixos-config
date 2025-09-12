d01afeac8bacb27c33d3700f5a4800f318f8366d
A2Zkiller <zureikatabdullah@gmail.com>
2025-09-12 18:43:58 -0400

    Fixed some stuff

--- modules/config/constants.nix
+++ modules/config/constants.nix
@@ -1,4 +1,8 @@
-{ delib, ... }:
+{
+  delib,
+  pkgs,
+  ...
+}:
 delib.module {
   name = "constants";
 
@@ -6,5 +10,9 @@ delib.module {
     username = readOnly (strOption "a2z");
     userfullname = readOnly (strOption "Abdullah Zureikat");
     useremail = readOnly (strOption "zureikatabdullah@gmail.com");
+
+    wallpaper = pathOption ./.;
+    polarity = strOption "dark";
+    base16Scheme = strOption "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
   };
 }
