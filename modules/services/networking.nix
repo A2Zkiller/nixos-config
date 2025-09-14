146b678c80138eccc5cc7055f2cc9fd1a9a7f60c
A2Zkiller <zureikatabdullah@gmail.com>
2025-09-14 19:19:49 +0000

    Changed networking hostname

--- modules/services/networking.nix
+++ modules/services/networking.nix
@@ -1,16 +1,19 @@
-{ delib, ... }:
+{
+  delib,
+  host,
+  ...
+}:
 delib.module {
   name = "services.networking";
 
   options = delib.singleEnableOption true;
 
-  nixos.ifEnabled =
-    { myconfig, ... }:
-    {
-      networking = {
-        hostName = myconfig.constants.username;
-        networkmanager.enable = true;
-        firewall.enable = true;
-      };
+  nixos.ifEnabled = {myconfig, ...}: {
+    networking = {
+      # hostName = myconfig.constants.username;
+      hostName = host.name;
+      networkmanager.enable = true;
+      firewall.enable = true;
     };
+  };
 }
