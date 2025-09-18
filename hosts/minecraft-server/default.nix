ba21a0c68938451e85c7a5d445e5db82ec414507
A2Zkiller <zureikatabdullah@gmail.com>
2025-09-18 01:28:34 +0000

    Fixes

--- hosts/minecraft-server/default.nix
+++ hosts/minecraft-server/default.nix
@@ -12,6 +12,4 @@ delib.host {
       main.enable = true;
     };
   };
-
-  # nix run nixpkgs#nixos-anywhere --flake .#minecraft-server --generate-hardware-config nixos-generate-config ./hosts/minecraft-server/hardware-configuration.nix root@216.45.53.180
 }
--- modules/server/openssh.nix
+++ modules/server/openssh.nix
@@ -4,11 +4,15 @@ delib.module {
 
   options = delib.singleEnableOption true;
 
-  nixos.ifEnabled = {myconfig, ...}: {
+  nixos.ifEnabled = {myconfig, ...}: let
+    keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1gE5OV3FOZ5KGCEr/9kVo44vgw81DF7TGGskM6srMy zureikatabdullah@gmail.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEb1z7g0oTbmhxMkzxO5ZeADY62ALEjF8HSRSgTu7IHA a2z@vmware"
+    ];
+  in {
     services.openssh.enable = true;
 
-    users.users.${myconfig.constants.username}.openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1gE5OV3FOZ5KGCEr/9kVo44vgw81DF7TGGskM6srMy zureikatabdullah@gmail.com"
-    ];
+    users.users.root.openssh.authorizedKeys.keys = keys;
+    users.users.${myconfig.constants.username}.openssh.authorizedKeys.keys = keys;
   };
 }
