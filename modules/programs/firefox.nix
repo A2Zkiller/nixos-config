1aa8fb376006c0b4c5123278b4020dd21591c00e
A2Zkiller <zureikatabdullah@gmail.com>
2026-06-16 02:54:03 -0400

    Added wrapped firefox to module

---
+++ modules/programs/firefox.nix
@@ -0,0 +1,8 @@
+{self, inputs, ...}: {
+  flake.nixosModules.librewolf = {pkgs, ...}: {
+    programs.firefox = {
+      enable = true;
+      package = self.packages.${pkgs.stdenv.hostPlatform.system}.myLibrewolf;
+    };
+  };
+}
