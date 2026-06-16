606950bf1a8035f7886171d9a5ef9b7455e63caa
A2Zkiller <zureikatabdullah@gmail.com>
2026-06-15 23:12:17 -0400

    Added wrapper to nh to add NH_FLAKE environment variable

--- modules/programs/nh.nix
+++ modules/programs/nh.nix
@@ -1,8 +1,19 @@
-{self, inputs, ...}: {
-  flake.nixosModules.nh = {...}: {
+{self, inputs, getSystem, ...}: {
+  flake.nixosModules.nh = {pkgs, ...}: {
     programs.nh = {
       enable = true;
       clean.enable = true;
+
+      package = (getSystem pkgs.stdenv.hostPlatform.system).packages.myNh;
     };
+  };
+
+  flake.wrappers.myNh = {pkgs, wlib, ...}: {
+    imports = [ wlib.modules.default ];
+
+    inherit pkgs;
+
+    drv = pkgs.nh;
+    env.NH_FLAKE = "/home/a2z/nixos-config";
   };
 }
