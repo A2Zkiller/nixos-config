3167dd91c76c5ff0d57111f9892810c177cf1d77
A2Zkiller <zureikatabdullah@gmail.com>
2026-06-16 00:53:09 -0400

    Attempt to fix fish wrapper

--- modules/wrappedPrograms/fish.nix
+++ modules/wrappedPrograms/fish.nix
@@ -5,20 +5,17 @@
 
       package = pkgs.fish;
 
-      settings = {
-        shellAliases = {
-          cat = "${lib.getExe pkgs.bat}";
-          ls = "${lib.getExe pkgs.eza} --icons -a --group-directories-first";
-          tree = "${lib.getExe pkgs.eza} --color=auto --tree";
-          grep = "${lib.getExe pkgs.gnugrep} --color=auto";
-          cd = "${lib.getExe pkgs.zoxide}";
-          lg = "${lib.getExe pkgs.lazygit}";
-        };
-
-        configFile.content = ''
-          ${lib.getExe pkgs.zoxide} init fish | source
-        '';
+      shellAliases = {
+        cat = "${lib.getExe pkgs.bat}";
+        ls = "${lib.getExe pkgs.eza} --icons -a --group-directories-first";
+        tree = "${lib.getExe pkgs.eza} --color=auto --tree";
+        grep = "${lib.getExe pkgs.gnugrep} --color=auto";
+        cd = "${lib.getExe pkgs.zoxide}";
+        lg = "${lib.getExe pkgs.lazygit}";
       };
+      configFile.content = ''
+        ${lib.getExe pkgs.zoxide} init fish | source
+      '';
     };
   };
 }
