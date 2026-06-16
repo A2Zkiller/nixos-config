c48e865c6aff5f4fac60b84b6ae4745635169b97
A2Zkiller <zureikatabdullah@gmail.com>
2026-06-16 13:39:05 -0400

    Added helix wrapped package

---
+++ modules/wrappedPrograms/helix.nix
@@ -0,0 +1,53 @@
+{self, inputs, ...}: {
+  perSystem = {pkgs, lib, self', ...}: {
+    packages.myHelix = inputs.wrappers.wrappers.helix.wrap {
+      inherit pkgs;
+
+      package = pkgs.helix;
+
+      runtimePkgs = [
+        pkgs.nil
+        pkgs.nixd
+        pkgs.alejandra
+
+        pkgs.marksman
+        pkgs.markdown-oxide
+      ];
+
+      settings = {
+        theme = "catppuccin_mocha";
+
+        editor = {
+          line-number = "relative";
+          cursor-shape = {
+            normal = "block";
+            insert = "bar";
+            select = "underline";
+          };
+        };
+      };
+
+      languages = {
+        language = [
+          {
+            name = "nix";
+            auto-format = true;
+            formatter.command = "${lib.getExe pkgs.alejandra}";
+            language-servers = [
+              "nixd"
+              "nil"
+            ];
+          }
+
+          {
+            name = "markdown";
+            language-servers = [
+              "marksman"
+              "markdown-oxide"
+            ];
+          }
+        ];
+      };
+    };
+  };
+}
