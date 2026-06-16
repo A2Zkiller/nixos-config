330b772470b50e2f0873e2ecf1375cc37f7fc520
A2Zkiller <zureikatabdullah@gmail.com>
2026-06-16 00:39:39 -0400

    Added wrapped Fish pkg

---
+++ modules/wrappedPrograms/fish.nix
@@ -0,0 +1,24 @@
+{inputs, ...}: {
+  perSystem = {pkgs, lib, self', ...}: {
+    packages.myFish = inputs.wrappers.wrappers.fish.wrap {
+      inherit pkgs;
+
+      package = pkgs.alacritty;
+
+      settings = {
+        shellAliases = {
+          cat = "${lib.getExe pkgs.bat}";
+          ls = "${lib.getExe pkgs.eza} --icons -a --group-directories-first";
+          tree = "${lib.getExe pkgs.eza} --color=auto --tree";
+          grep = "${lib.getExe pkgs.grep} --color=auto";
+          cd = "${lib.getExe pkgs.zoxide}";
+          lg = "${lib.getExe pkgs.lazygit}";
+        };
+
+        configFile.content = ''
+          ${lib.getExe pkgs.zoxide} init fish | source
+        '';
+      };
+    };
+  };
+}
