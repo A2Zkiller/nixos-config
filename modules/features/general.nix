4cb18c7234fc37d076eedaf50c784b4b8a1096be
A2Zkiller <zureikatabdullah@gmail.com>
2026-06-17 17:37:22 -0400

    Added America/New_York as default time zone

--- modules/features/general.nix
+++ modules/features/general.nix
@@ -3,12 +3,18 @@
   inputs,
   ...
 }: {
-  flake.nixosModules.general = {pkgs, ...}: let
+  flake.nixosModules.general = {
+    pkgs,
+    lib,
+    ...
+  }: let
     selfpkgs = self.packages.${pkgs.stdenv.hostPlatform.system};
   in {
     imports = [
       self.nixosModules.nix
     ];
+
+    time.timeZone = lib.mkDefault "America/New_York";
 
     environment.variables = {
       EDITOR = "hx";
