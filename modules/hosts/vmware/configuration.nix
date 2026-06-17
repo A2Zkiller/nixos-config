c74116ad62ffaed87b3db8c356f2ba2ee5827035
A2Zkiller <zureikatabdullah@gmail.com>
2026-06-17 18:14:13 -0400

    Removed redundant lines from vmware host's config

--- modules/hosts/vmware/configuration.nix
+++ modules/hosts/vmware/configuration.nix
@@ -10,63 +10,16 @@
     ...
   }: {
     imports = [
-      # Include the results of the hardware scan.
       self.nixosModules.myVmwareHardware
       self.nixosModules.vmwareGuestAdditions
       self.nixosModules.desktop
       self.nixosModules.grub
     ];
 
-    nix.settings.experimental-features = ["nix-command" "flakes"];
-
     networking.hostName = "myVmware"; # Define your hostname.
 
-    # Enable networking
-    networking.networkmanager.enable = true;
-
-    # Set your time zone.
     time.timeZone = "America/New_York";
 
-    # Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
-
-    i18n.extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-
-    services.displayManager.ly.enable = true;
-
-    # Enable sound with pipewire.
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-    };
-
-    # Define a user account. Don't forget to set a password with ‘passwd’.
-    users.users."a2z" = {
-      isNormalUser = true;
-      description = "a2z";
-      extraGroups = ["networkmanager" "wheel"];
-    };
-
-    # List packages installed in system profile. To search, run:
-    environment.systemPackages = with pkgs; [
-    ];
-
-    nixpkgs.config.allowUnfree = true;
-
-    system.stateVersion = "26.05"; # Did you read the comment?
+    system.stateVersion = "26.05";
   };
 }
