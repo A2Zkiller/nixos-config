7940615591ca6851d85fb63d4da13cec7307ba17
A2Zkiller <zureikatabdullah@gmail.com>
2026-06-18 02:18:43 -0400

    Removed old wrapper modules

--- flake.nix
+++ flake.nix
@@ -9,8 +9,6 @@
       url = "github:BirdeeHub/nix-wrapper-modules";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
