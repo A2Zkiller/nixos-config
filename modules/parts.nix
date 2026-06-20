a2abf1b647ebe029841a1b5fc0a108171af7abda
A2Zkiller <zureikatabdullah@gmail.com>
2026-06-20 16:47:51 -0400

    Made perSystem use the flake's nixpkgs

--- modules/parts.nix
+++ modules/parts.nix
@@ -1,10 +1,18 @@
 {
-  config = {
-    systems = [
-      "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "aarch64-darwin"
-    ];
+  self,
+  inputs,
+  ...
+}: {
+  systems = [
+    "x86_64-linux"
+    "x86_64-darwin"
+    "aarch64-linux"
+    "aarch64-darwin"
+  ];
+
+  perSystem = {system, ...}: {
+    _module.args.pkgs = import inputs.nixpkgs {
+      inherit system;
+    };
   };
 }
