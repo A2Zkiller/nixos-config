4ccdb8ee3f1b24eae6bf209688bc9d6e00e141af
A2Zkiller <zureikatabdullah@gmail.com>
2026-06-26 22:25:15 -0400

    Changed yazi look

--- modules/wrappedPrograms/yazi.nix
+++ modules/wrappedPrograms/yazi.nix
@@ -19,6 +19,14 @@
       };
 
       settings = {
+        yazi = {
+          mgr = {
+            show_hidden = true;
+            show_symlink = true;
+            sort_dir_first = true;
+          };
+        };
+
         theme = {
           flavor.dark = "catppuccin-mocha";
         };
