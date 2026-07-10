67bac9c909e5c09b22bc14368bdf89b6485c045f
A2Zkiller <zureikatabdullah@gmail.com>
2026-07-09 21:44:12 -0400

    Setup Hjem to work with emacs config

--- modules/programs/emacs.nix
+++ modules/programs/emacs.nix
@@ -1,5 +1,12 @@
 {self, ...}: {
-  flake.nixosModules.emacs = {pkgs, ...}: {
+  flake.nixosModules.emacs = {
+    pkgs,
+    lib,
+    config,
+    ...
+  }: let
+    user = config.preferences.user.name;
+  in {
     environment.systemPackages = [
       pkgs.emacs
 
@@ -11,5 +18,26 @@
     };
 
     # TODO: Setup Hjem emacs config from github
+    hjem.users.${user} = {
+      enable = true;
+
+      # into ~/.config/emacs
+      xdg.config.files.emacs = {
+        source = pkgs.fetchFromGitHub {
+          owner = "A2Zkiller";
+          repo = "emacs-config";
+          rev = "833a21f";
+          hash = lib.fakeHash;
+        };
+
+        type = "directory";
+        permissions = "644";
+        clobber = false;
+      };
+    };
+
+    imports = [
+      self.nixosModules.hjem
+    ];
   };
 }
