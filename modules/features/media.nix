7de08d4e040b432ad9132f4ddc9a7addcbb8e237
A2Zkiller <zureikatabdullah@gmail.com>
2026-07-13 18:11:39 -0400

    Set xdg mime types for librewolf and media apps (feh and mpv)

--- modules/features/media.nix
+++ modules/features/media.nix
@@ -4,5 +4,14 @@
       pkgs.mpv
       pkgs.feh
     ];
+
+    xdg.mime.defaultApplications = let
+      images = "feh.desktop";
+      videos = "mpv.desktop";
+    in {
+      "images/*" = images;
+      "video/*" = videos;
+      "audio/*" = videos;
+    };
   };
 }
--- modules/programs/librewolf.nix
+++ modules/programs/librewolf.nix
@@ -8,5 +8,16 @@
       enable = true;
       package = self.packages.${pkgs.stdenv.hostPlatform.system}.myLibrewolf;
     };
+
+    xdg.mime.defaultApplications = let
+      browsers = "librewolf.desktop;firefox.desktop";
+    in {
+      "text/html" = browsers;
+      "text/xml" = browsers;
+      "application/vnd.mozilla.xul+xml" = browsers;
+      "application/xhtml+xml" = browsers;
+      "x-scheme-handler/https" = browsers;
+      "x-scheme-handler/http" = browsers;
+    };
   };
 }
